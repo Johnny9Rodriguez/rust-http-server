97d572dde407f3532842582822a49694b2a7e725
Joe Pytlik <joe@fello.fish>
2025-08-16 01:53:42 +0200

    read 8 bytes buffer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    fs::File,
+    io::{BufReader, Read, Result},
+};
+
+fn main() -> Result<()> {
+    let file = File::open("messages.txt")?;
+    let mut buf_reader = BufReader::new(file);
+
+    loop {
+        let mut s = String::new();
+        let bytes_read = buf_reader.by_ref().take(8).read_to_string(&mut s)?;
+
+        if bytes_read == 0 {
+            break;
+        }
+
+        println!("read: {s}")
+    }
+
+    Ok(())
 }
